4a72a7f24ca9d7e7163592d1e05d0a66a107b24b
Tina Humphrey <humphrey14t@gmail.com>
2024-03-22 18:46:13 -0400

    commit changes to hero files for animation and added parallax folder and files

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -3,26 +3,66 @@
 //Importing Hero from the hero folder
 import "./hero.scss";
 
+//Importing Motion
+import {motion} from "framer-motion";
+
+const textVariants = {
+    initial: {
+        x: -500,
+        opacity: 0,
+    },
+    animate: {
+        x: 0,
+        opacity: 1,
+        transition: {
+            duration: 1,
+            staggerChildren: 0.1,
+        },
+    },
+    scrollButton:{
+        opacity: 0,
+        y: 10,
+        transition:{
+            duration: 2,
+            repeat:Infinity
+        }
+    },
+};
+
+const sliderVariants = {
+    initial: {
+        x: 0,
+    },
+    animate: {
+        x: "-220%",
+        transition: {
+            repeat:Infinity,
+            repeatType: "mirror",
+            duration: 20,
+        },
+    },
+};
+
 const Hero = () => {
     
     return (
         <div className="hero">
             <div className="wrapper">
-                <div className="textContainer">
-                    <h2>TINA HUMPHREY</h2>
-                    <h1>Full Stack Web Developer</h1>
-                    <div className="buttons">
-                        <button>View Latest Projects</button>
-                        <button>Contact Me</button>
-                    </div>
-                <img src ="/scroll.png" alt="Scroll Icon" />
-                </div> 
+                <motion.div className="textContainer" variants={textVariants} initial="initial" animate="animate">
+                    <motion.h2 variants={textVariants}>TINA HUMPHREY</motion.h2>
+                    <motion.h1 variants={textVariants}>Full Stack Web Developer</motion.h1>
+                    <motion.div variants={textVariants} className="buttons">
+                        <motion.button variants={textVariants}>View Latest Projects</motion.button>
+                        <motion.button variants={textVariants}>Contact Me</motion.button>
+                    </motion.div>
+                <motion.img variants={textVariants} animate="scrollButton" src ="/scroll.png" alt="Scroll Icon" />
+                </motion.div> 
             </div> 
-            <div className="slidingTextContainer">
-                Travel Enthusiast
-            </div>
+            < motion.div className="slidingTextContainer" variants={sliderVariants} initial="initial" animate="animate">
+                Coding Expert
+            </motion.div>
             <div className="imageContainer">
-                <img src="/hero.png" alt="Hero" />
+                <img src="/hero.png" alt="Hero Image" />
             </div>  
         </div>
     );
