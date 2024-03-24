63f926ede5f9e2a47fe31829afdd2875cd98a645
Tina Humphrey <humphrey14t@gmail.com>
2024-03-23 20:31:54 -0400

    commit changes to hero.jsx, navbar.jsx, parallax files and added png pics to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 // Bringing in the required import app.scss
 import "./app.scss";
-import Navbar from "./components/navbar/Navbar";
 import Hero from "./components/hero/Hero";
+import Navbar from "./components/navbar/Navbar";
+import Parallax from "./components/parallax/Parallax";
 
 const App = () => {
     return <div>
@@ -9,10 +10,12 @@ const App = () => {
             <Navbar/>
             <Hero/>
         </section>
-        <section id="Skills">Skills</section>
-        <section id="Projects">Projects</section>
-        <section id="Resume">Resume</section>
-        <section id="Contact">Contact</section>
+        <section id ="Skills"><Parallax type="skills"/></section>
+        <section>Skills</section>
+        <section id="Projects"><Parallax type="projects"/></section>
+        <section>Projects</section>    
+        <section>Resume</section>
+        <section>Contact</section>
         <section id="About">About</section>
     </div>;
   };
--- src/components/parallax/Parallax.jsx
+++ src/components/parallax/Parallax.jsx
@@ -1,9 +1,16 @@
-import "./parallax.scss"
+import "./parallax.scss";
+import {motion} from "framer-motion";
 
-const Parallax = () => {
+const Parallax = ({type}) => {
     return (
-        <div>Parallax</div>
-    )
-}
+        <div className="parallax" style={{background:type === "skills" 
+        ? "linear-gradient(180deg, #111132, #0c0c1d)" 
+        : "linear-gradient(180deg, #111132, #505064)",
+        }}>
+            <motion.h1>{type==="skills" ? "What I Can Offer?" : "View My Latests Projects"}</motion.h1>
+            <motion.div className="mountains"></motion.div>
+        </div>
+    );
+};
 
 export default Parallax;
