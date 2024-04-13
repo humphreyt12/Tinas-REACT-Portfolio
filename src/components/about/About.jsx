c55d678dc01aebfadee4b40475f4c902068cd0a6
Tina Humphrey <humphrey14t@gmail.com>
2024-04-12 23:59:37 -0400

    commit modified change to About.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -21,7 +21,7 @@ const y = useTransform(scrollYProgress, [0,1], ["0%", "-300%"]);
                     Prior to the bootcamp, I studied Java, Python and Object Oriented Programming from my Business Technology minor at University of Miami (Go Canes 💚🧡!). 
                     I have a Master's of Science in Leadership & Management with a concentration in HR from Purdue Global. I currently work at First Light HomeCare ☘️ as a HR Specialist. 
                     I'm currently working on an app that will help children develop their math, reading, science and vocabulary skills.
-                    After the bootcamp, I plan to tranition into a software development career and create a team to help me launch my app. 
+                    After the bootcamp, I plan to transition into a software development career and create a team to help me launch my app. 
                     Contact me below if you want to collaborate together and join my team! 
                     I thrive in ambitious and passionate roles that allow me to contribute my solution-focused and innovative mindset. My hobbies include traveling, journaling, ZUMBA, kickboxing, reading, and watching movies. ✈️📖💃🏾🥊🎬
                     Please scroll down or click the sidebar to view the project section to see some samples of my work. 
